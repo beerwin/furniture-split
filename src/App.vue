326aa218196cc7bee7267793306bbc99a0a004b9
Ervin Nagy <nagyervin.bws@gmail.com>
2025-11-11 15:34:13 +0200

    make main menu always visible

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@ import MainMenu from './components/navigation/mainMenu.vue'
 </script>
 
 <template>
-  <div class="bg-white dark:bg-gray-900 shadow-2xl shadow-neutral-400 dark:shadow-gray-950 text-neutral-900 dark:text-neutral-200 flex">
+  <div class="fixed w-full bg-white dark:bg-gray-900 shadow-2xl shadow-neutral-400 dark:shadow-gray-950 text-neutral-900 dark:text-neutral-200 flex">
     <div class="flex-grow-1">
       <MainMenu></MainMenu>
     </div>
   </div>
-  <div class="bg-white dark:bg-gray-950 text-neutral-900 dark:text-neutral-200 flex flex-col md:flex-row">
+  <div class="pt-10 bg-white dark:bg-gray-950 text-neutral-900 dark:text-neutral-200 flex flex-col md:flex-row">
     <CutsForm class="flex-4"></CutsForm>
     <CutsResults class="flex-1"></CutsResults>
   </div>
